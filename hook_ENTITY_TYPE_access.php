6a005930a57bd016138f0116cfa1e0e26e95caae
Volodymyr <49057003+vovashvets@users.noreply.github.com>
2021-02-16 13:14:58 +0200

    hook_ENTITY_TYPE_access

---
+++ hook_ENTITY_TYPE_access.php
@@ -0,0 +1,36 @@
+<?php
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Implements hook_ENTITY_TYPE_access() for entity type "user".
+ *
+ * @param \Drupal\user\UserInterface $entity
+ *   The user object to check access for.
+ *
+ * @return
+ *   Permission.
+ */
+function MODULE_user_access(UserInterface $entity, $op, AccountInterface $account) {
+  // Disable access to user pages.
+  if ($entity->id() == $account->id()) {
+    switch ($op) {
+      case 'update':
+      case 'view':
+      case 'user_name':
+      case 'user_mail':
+      case 'user_pass':
+      case 'user_edit':
+      case 'user_delete':
+        // Example of limiting access to known user pages by role.
+        $user_roles = $account->getRoles();
+        if (!in_array('super_admin', $user_roles)) {
+          return AccessResult::forbidden();
+        }
+        break;
+    }
+  }
+  // No opinion.
+  return AccessResult::neutral();
+}
