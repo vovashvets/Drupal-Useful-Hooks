70d11bec33c4e1e4223220fcaac8cc8982648463
Volodymyr <49057003+vovashvets@users.noreply.github.com>
2021-04-12 12:10:22 +0300

    Delete hook_preprocess_HOOK.php

--- hook_preprocess_HOOK.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Implements hook_preprocess_HOOK().
- */
-function MODULENAME_preprocess_TEMPLATENAME(&$variables) {
-  unset($variables['links']['it'], $variables['links']['fr']);
-}
